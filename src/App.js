2c5aada1423b350ff8eeca40ac4126d027c6b367
Kristi Parrish <kp@Kristis-MacBook-Pro.local>
2019-12-09 17:00:22 -0700

    MVP + Stretch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -8,6 +8,10 @@ function App() {
 
   const [valueHome, setCountHome] = useState(0)
   const [valueAway, setCountAway] = useState(0)
+  
+  // const Timer = () => {
+  //   const [seconds, setSeconds] = useState(0);
+  //   const [isActive, setIsActive] = useState(false);
 
   return (
     <div className="container">
@@ -20,24 +24,32 @@ function App() {
 
             <div className="home__score"> {valueHome} </div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">0:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score"> {valueAway} </div>
           </div>
         </div>
+
+
         <BottomRow />
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => setCountHome(valueHome + 7)}>Home Touchdown</button>
-
+          <button className="homeButtons__touchdown" onClick={() => setCountHome(valueHome + 6)}>Home Touchdown</button>
+          <button className="homeButtons__pat" onClick={() => setCountHome(valueHome + 1)}>Home PAT</button>
           <button className="homeButtons__fieldGoal" onClick={() => setCountHome(valueHome + 3)}>Home Field Goal</button>
+          <button className="homeButtons__twopoints" onClick={() => setCountHome(valueHome + 2)}>Home 2PT Con/Safety</button>
+          <button className="homeButtons__reset" onClick={() => setCountHome(0)}>Reset Home</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setCountAway(valueAway + 7)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => setCountAway(valueAway + 6)}>Away Touchdown</button>
+          <button className="awayButtons__pat" onClick={() => setCountAway(valueAway + 1)}>Away PAT</button>
           <button className="awayButtons__fieldGoal" onClick={() => setCountAway(valueAway + 3)}>Away Field Goal</button>
+          <button className="awayButtons__twopoints" onClick={() => setCountAway(valueAway + 2)}>Away 2PT Con/Safety</button>
+          <button className="awayButtons__reset" onClick={() => setCountAway(0)}>Reset Away</button>
+
         </div>
       </section>
     </div>
